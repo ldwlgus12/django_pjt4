295a8f88f3eb256fb8e73b44a529da4a07d268fa
hyundol2 <nhs503601@gmail.com>
2023-04-20 17:45:06 +0900

    hyeonseok

--- balances/forms.py
+++ balances/forms.py
@@ -6,6 +6,19 @@ class BalanceForm(forms.ModelForm):
         model = Balance
         fields = ('title', 'select1_content', 'select2_content', 'image_1', 'image_2',)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs['class'] = 'form-control'
+        # self.fields['title'].widget.attrs['placeholder'] = '제목'
+        self.fields['select1_content'].widget.attrs['class'] = 'form-control'
+        # self.fields['select1_content'].widget.attrs['placeholder'] = '첫 번째 주제'
+
+        self.fields['select2_content'].widget.attrs['class'] = 'form-control'
+        # self.fields['select2_content'].widget.attrs['placeholder'] = '두 번째 주제'
+
+        self.fields['image_1'].widget.attrs['class'] = 'form-control'
+        self.fields['image_2'].widget.attrs['class'] = 'form-control'
 
 
 class CommentForm(forms.ModelForm):
