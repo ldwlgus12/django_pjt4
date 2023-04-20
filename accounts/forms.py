7c1d476356c37c30e1dce9ec4fc7c3f23a4d67aa
Choi-jw-96 <won60917@naver.com>
2023-04-20 18:28:38 +0900

    LoingUdateSignup

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,12 +6,85 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = get_user_model()
         fields = ('username', 'email', 'password1', 'password2',)
+    username = forms.CharField(
+        required=True,
+        label='ID',
+        widget= forms.TextInput(
+          attrs = {
+            'class' : 'form-control',
+            'placeholder' : '영문 소문자 또는 영문 대문자, 숫자 조합 6~12 자리',
+            'style' : 'color:rgb(102, 77, 3);'
+                      'border-color:rgb(230, 217, 179);',
+          }
+        )
+    )
+    email = forms.EmailField(
+        required=True,
+        label='Email',
+        widget= forms.EmailInput(
+          attrs = {
+            'class' : 'form-control',
+            'placeholder' : 'Email 입력',
+            'style' : 'color:rgb(102, 77, 3);'
+                      'border-color:rgb(230, 217, 179);',
+          }
+        )
+    )
+    password1 = forms.CharField(
+        required=True,
+        label='Password1',
+        widget= forms.PasswordInput(
+          attrs = {
+            'class' : 'form-control',
+            'placeholder' : '영문, 숫자, 특수문자(~!@#$%^&*) 조합 8~15 자리',
+            'style' : 'color:rgb(102, 77, 3);'
+            'border-color:rgb(230, 217, 179);',
+          }
+        )
+    )
+    password2 = forms.CharField(
+        required=True,
+        label='Password2',
+        widget= forms.PasswordInput(
+          attrs = {
+            'class' : 'form-control',
+            'placeholder' : '영문, 숫자, 특수문자(~!@#$%^&*) 조합 8~15 자리',
+            'style' : 'color:rgb(102, 77, 3);'
+            'border-color:rgb(230, 217, 179);',
+          }
+        )
+    )
+
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm):
         model = get_user_model()
         fields = ('username', 'email',)
+    username = forms.CharField(
+        required=True,
+        label='ID',
+        widget= forms.TextInput(
+          attrs = {
+            'class' : 'form-control',
+            'placeholder' : 'ID 입력',
+            'style' : 'color:rgb(102, 77, 3);'
+                      'border-color:rgb(230, 217, 179);',
+          }
+        )
+    )
+    email = forms.EmailField(
+        required=True,
+        label='Email',
+        widget= forms.EmailInput(
+          attrs = {
+            'class' : 'form-control',
+            'placeholder' : 'Email 입력',
+            'style' : 'color:rgb(102, 77, 3);'
+                      'border-color:rgb(230, 217, 179);',
+          }
+        )
+    )
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
@@ -21,16 +94,20 @@ class CustomAuthenticationForm(AuthenticationForm):
         widget= forms.TextInput(
           attrs = {
             'class' : 'form-control',
-            'placeholder' : 'ID 입력'
+            'placeholder' : 'ID 입력',
+            'style' : 'color:rgb(102, 77, 3);'
+                      'border-color:rgb(230, 217, 179);',
           }
         )
     )
     password = forms.CharField(
-        label='pssword',
+        label='Password',
         widget= forms.PasswordInput(
           attrs = {
             'class' : 'form-control',
-            'placeholder' : 'password 입력'
+            'placeholder' : 'Password 입력',
+            'style' : 'color:rgb(102, 77, 3);'
+                      'border-color:rgb(230, 217, 179);',
           }
         )
     )
